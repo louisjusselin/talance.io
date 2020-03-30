3a3dba6fda3790a67f22f2643e94b7b3063fe5b2
HomeGarden <>
2020-03-30 02:06:31 +0200

    12

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,27 +102,27 @@
   });
 
   // Gallery carousel (uses the Owl Carousel library)
-  $(".gallery-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    center: true,
-    margin: 25,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 3
-      },
-      992: {
-        items: 4
-      },
-      1200: {
-        items: 5
-      }
-    }
-  });
+  // $(".gallery-carousel").owlCarousel({
+  //   autoplay: true,
+  //   dots: true,
+  //   loop: true,
+  //   center: true,
+  //   margin: 25,
+  //   responsive: {
+  //     0: {
+  //       items: 1
+  //     },
+  //     768: {
+  //       items: 3
+  //     },
+  //     992: {
+  //       items: 4
+  //     },
+  //     1200: {
+  //       items: 5
+  //     }
+  //   }
+  // });
 
   // Initiate venobox lightbox
   $(document).ready(function() {
@@ -130,22 +130,22 @@
   });
 
   // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 1
-      },
-      900: {
-        items: 2
-      }
-    }
-  });
+  // $(".testimonials-carousel").owlCarousel({
+  //   autoplay: true,
+  //   dots: true,
+  //   loop: true,
+  //   responsive: {
+  //     0: {
+  //       items: 1
+  //     },
+  //     768: {
+  //       items: 1
+  //     },
+  //     900: {
+  //       items: 2
+  //     }
+  //   }
+  // });
 
   // Initi AOS
   AOS.init({
